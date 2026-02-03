2e6a7a16e5514d3b30e34d5436aeb4d402a4a154
Carmen Rianna <252221102+carmenrianna@users.noreply.github.com>
2026-02-04 00:59:17 +0800

    Abstract

---
+++ abstract_practices.java
@@ -0,0 +1,31 @@
+public class abstract_practices {
+    public static void main() {
+        Guitar gtr = new Guitar();
+        Piano piano = new Piano();
+
+        System.out.println(piano.build);
+        piano.play();
+    }
+
+    abstract static class Instrument {
+        String build = "Wood";
+
+        abstract void play();
+    }
+
+    static class Guitar extends Instrument {
+        int strings = 6;
+        @Override
+        void play() {
+            System.out.println("The guitar is playing.");
+        }
+    }
+
+    static class Piano extends Instrument {
+        int keys = 88;
+        @Override
+        void play() {
+            System.out.println("The piano is playing.");
+        }
+    }
+}
