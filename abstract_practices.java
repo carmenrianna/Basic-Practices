efd6f952826a7aba4a76ee64ac6c783b9de26aea
Carmen Rianna <252221102+carmenrianna@users.noreply.github.com>
2026-02-04 00:48:38 +0800

    Inheritance and Overriding

---
+++ abstract_practices.java
@@ -0,0 +1,34 @@
+public class abstract_practices {
+    public static void main() {
+        Instrument ins = new Instrument();
+        Guitar gtr = new Guitar();
+        Piano piano = new Piano();
+
+        System.out.println(piano.build);
+        gtr.play();
+    }
+
+    public static class Instrument {
+        String build = "Wood";
+
+        void play() {
+            System.out.println("I am playing.");
+        }
+    }
+
+    static class Guitar extends Instrument {
+        int strings = 6;
+        @Override
+        void play() {
+            System.out.println("The guitar is playing.");
+        }
+    }
+
+    static class Piano extends Instrument {
+        int keys = 88;
+        @Override
+        void play() {
+            System.out.println("The piano is playing.");
+        }
+    }
+}
